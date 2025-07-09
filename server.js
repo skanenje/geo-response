805b1a30371f531192b8cdf280c5c01c949bbdc7
skanenje <swapomuse@gmail.com>
2025-07-09 15:02:32 +0300

    feat(server): setup listening port and load locations from file

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import express from 'express';  
 import cors from 'cors';
+import fs from 'fs';
 
 const app = express();
 const PORT = 3000;
 
+// Load blackspot data (hardcoded values)
+const zones = JSON.parse(fs.readFileSync('./data/zones.json', 'utf-8'));
+
+
 app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
