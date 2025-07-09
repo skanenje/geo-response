78988558e6328745b77efe433f233d04c8a8a2d4
skanenje <swapomuse@gmail.com>
2025-07-09 15:12:07 +0300

    feat: implement a get endpoint that return a list of nearby areas from a given location

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ const zones = JSON.parse(fs.readFileSync('data/zone.json', 'utf-8'));
 
 app.use(cors());
 
+app.get('/zones/nearby', (req, res) => {
+  const { lat, lng } = req.query;
+
+  if (!lat || !lng) {
+    return res.status(400).json({ error: "Missing 'lat' or 'lng' query parameters" });
+  }
+
+  const userLat = parseFloat(lat);
+  const userLng = parseFloat(lng);
+
+  const radius = 1; // km
+  const nearby = zones
+    .map(zone => {
+      const dist = haversineDistance(userLat, userLng, zone.lat, zone.lng);
+      return dist <= radius ? { ...zone, distance_km: +dist.toFixed(3) } : null;
+    })
+    .filter(Boolean)
+    .sort((a, b) => a.distance_km - b.distance_km);
+
+  res.json({ nearby_zones: nearby });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
